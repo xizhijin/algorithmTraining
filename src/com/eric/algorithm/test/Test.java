d125e3a31f741fd5c2e9ee3977ad35ab861819f2
eric <xizhijin@yeah.net>
2016-07-05 13:12:22 +0800

    this is a litter change

--- src/com/eric/algorithm/test/Test.java
+++ src/com/eric/algorithm/test/Test.java
@@ -3,7 +3,7 @@ package com.eric.algorithm.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test!!!");
+		System.out.println("This is a test!");
 	}
 
 }
