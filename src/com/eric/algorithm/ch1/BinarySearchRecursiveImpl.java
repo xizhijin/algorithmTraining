b17deeefdb479001ed129117816ca7549c8861e2
eric <xizhijin@yeah.net>
2016-07-07 14:32:30 +0800

    fix a bug

--- src/com/eric/algorithm/ch1/BinarySearchRecursiveImpl.java
+++ src/com/eric/algorithm/ch1/BinarySearchRecursiveImpl.java
@@ -1,11 +1,12 @@
 package com.eric.algorithm.ch1;
 
 public class BinarySearchRecursiveImpl {
+	private static int deep = 0;
 
 	public static void main(String[] args) {
-		int[] array = {1, 3, 4, 5, 7, 8};
-		int key =4;
-		System.out.println(binarySearch(key, array));
+		int[] array = {1, 3, 4, 5, 7, 8, 9, 10};
+		int key =7;
+		System.out.println("The index of key is: " + binarySearch(key, array));
 	}
 
 	private static int binarySearch(int key, int[] array) {
@@ -13,15 +14,15 @@ public class BinarySearchRecursiveImpl {
 	}
 
 	private static int recursiveSearch(int key, int[] array, int lo, int hi) {
+		System.out.println("The deep is: " + ++deep + " and lo is: " + lo + ", hi is " + hi);
 		int mid = (lo + hi) / 2;
 		if (lo > hi) {
 			return -1;
 		} else if (key < array[mid]) {
-			recursiveSearch(key, array, lo, mid - 1);
+			return recursiveSearch(key, array, lo, mid - 1);
 		} else if (key > array[mid]) {
-			recursiveSearch(key, array, mid + 1, hi);
+			return recursiveSearch(key, array, mid + 1, hi);
 		} else return mid;
-		return -1;
 	}
 
 }
